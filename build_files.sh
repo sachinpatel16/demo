b3a79da6b93d23b7c8198ffeaebd9472dd1edb8e
sachin patel <134909296+sachinpatel16@users.noreply.github.com>
2025-02-02 11:45:39 +0530

    ee

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,8 @@ python3.9 -m pip install -r requirements.txt
 mkdir -p staticfiles_build
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear --verbosity 3
+
+ls -lah staticfiles_build 
 
 echo "BUILD END"

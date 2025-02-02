d32cee1241a14ef4ebf6d0a6a7e8c25cf4a56282
sachin patel <134909296+sachinpatel16@users.noreply.github.com>
2025-02-02 11:42:19 +0530

    ww

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,8 @@ echo "BUILD START"
 # Install dependencies
 python3.9 -m pip install -r requirements.txt
 
+mkdir -p staticfiles_build
+
 # Collect static files
 python3.9 manage.py collectstatic --noinput --clear
 

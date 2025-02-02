51b5ffdbfbb11eb992dc4b5a07e7cb893be9ccb3
sachin patel <134909296+sachinpatel16@users.noreply.github.com>
2025-02-02 13:01:39 +0530

    qq

--- build_files.sh
+++ build_files.sh
@@ -15,7 +15,7 @@ python3.9 -m pip install -r requirements.txt
 mkdir -p staticfiles
 
 # Collect static files (ensure it's pointing to the correct directory)
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput --clear 
 # Run migrations
 python3.9 manage.py migrate
 

b1fd4e1f4fdcf83c21e5a1898945bb8fbc926dff
sachin patel <134909296+sachinpatel16@users.noreply.github.com>
2025-02-02 12:00:06 +0530

    dd

--- demo/settings.py
+++ demo/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 

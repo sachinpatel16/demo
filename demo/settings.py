9316fecadb7f4cb2b7cf12fbb997377e8df9d207
sachin patel <134909296+sachinpatel16@users.noreply.github.com>
2025-02-02 12:43:39 +0530

    dd

--- demo/settings.py
+++ demo/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
